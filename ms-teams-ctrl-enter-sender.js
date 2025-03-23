ae1879f5f3d059c187b7a540ff1861005886e1e4
emptybe <282865064@qq.com>
2025-03-23 15:07:32 +0800

    Update ms-teams-ctrl-enter-sender.js

--- ms-teams-ctrl-enter-sender.js
+++ ms-teams-ctrl-enter-sender.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         微软Teams使用Ctrl+Enter发送消息(MS Teams Ctrl+Enter Sender)
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/emptybe
 // @version      1.0(2025-03-20)
 // @description  在微软Teams里面通过邮件输入模式输入消息,点击Enter不会发送,点击Ctrl+Enter才会发送;当鼠标点击微软Teams的消息输入框时,脚本会自动将文本输入模式变更为邮件输入模式,以此来达到Ctrl+Enter发送消息的目的(In Microsoft Teams through the mail input mode to enter the message, click Enter will not be sent, click Ctrl + Enter will be sent; when the mouse clicks on the Microsoft Teams message input box, the script will automatically change the text input mode to the mail input mode, in order to achieve the purpose of Ctrl + Enter to send the message.)
 // @author       zero-empty
